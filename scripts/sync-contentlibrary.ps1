707611439f0f93f95657e9c90357946f90feb218
Scott R Bowe <scottb@sentania.net>
2025-03-21 14:46:02 -0500

    powershell version for local usage

---
+++ scripts/sync-contentlibrary.ps1
@@ -0,0 +1,23 @@
+﻿Param(
+  [string]$vSphereUSERNAME,
+  [string]$vSpherePASSWORD
+)
+Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+Find-Module "VMware.PowerCLI" | Install-Module -Scope "CurrentUser" -AllowClobber
+Import-Module "VMware.PowerCLI"
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -confirm:$false
+$vCenterlist = get-content inputs/vcenterlist.txt
+
+foreach ($vcenter in $vCenterlist)
+{
+$cisServerConnection = Connect-CisServer -Server $vcenter -User $vSphereUSERNAME -Password $vSpherePASSWORD
+
+$contentlibrarysubscribedService = Get-CisService com.vmware.content.subscribed_library
+foreach ($library in $contentlibrarysubscribedService.list() )
+{
+  $libraryID =  $library.value
+  $contentlibrarysubscribedService.sync($libraryID)
+}
+
+Disconnect-CisServer -Server $cisServerConnection -Confirm:$false
+}
